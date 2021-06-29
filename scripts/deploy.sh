548d17bb33637cd160c42de16b66d75609e2aa41
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-06-29 17:51:56 +0200

    add relapath in manifest templates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,20 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 npx graph-compiler                                      \
   --config ${CONFIG:-configs/sample.json}               \
   --export-subgraph                                     \
-  --export-schema                                       &
+  --export-schema                                       &&
 
 npx graph codegen                                       \
-  ${SUBGRAPH:-generated/sample.subgraph.yaml}           &
+  ${SUBGRAPH:-generated/sample.subgraph.yaml}           &&
+
+npx graph build                                         \
+  ${SUBGRAPH:-generated/sample.subgraph.yaml}           &&
 
 npx graph deploy                                        \
   --ipfs ${IPFSNODE:-https://api.thegraph.com/ipfs/}    \
   --node ${GRAPHNODE:-https://api.thegraph.com/deploy/} \
   ${NAME:-amxx/sandbox}                                 \
-  ${SUBGRAPH:-generated/sample.subgraph.yaml}           &
+  ${SUBGRAPH:-generated/sample.subgraph.yaml}           &&
 
 echo 'done.'
